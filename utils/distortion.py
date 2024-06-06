71f1aa3f8008ec3d7aadffeed588d3c105afd504
Xiaole Alex Jiang <xjiang2@gradcenter.cuny.edu>
2024-06-06 12:17:43 -0400

    class data structure

--- utils/distortion.py
+++ utils/distortion.py
@@ -67,8 +67,8 @@ def sparse_transform_amplitude(x_length, y_length, num_of_terms, amplitude = 1, 
       A_nm = amplitude * rng.dirichlet(np.ones(num_of_terms)) * A_sign
       B_nm = amplitude * rng.dirichlet(np.ones(num_of_terms)) * B_sign
     elif alpha != None:
-      A_nm = amplitude * rng.dirichlet(alpha) * A_sign
-      B_nm = amplitude * rng.dirichlet(alpha) * B_sign
+      A_nm = amplitude * rng.dirichlet(alpha * np.ones(num_of_terms)) * A_sign
+      B_nm = amplitude * rng.dirichlet(alpha * np.ones(num_of_terms)) * B_sign
 
     A_nm = np.pad(A_nm, (0, pad_len), 'constant')
     A_nm = rng.permutation(A_nm).reshape(x_length, y_length)
@@ -90,10 +90,7 @@ def jacobian_det(x_map, y_map):
     there could be a minus sign because of transpose
     x, y index is confusing
   '''
-  x_length, y_length = x_map.shape
-  x = np.arange(x_length)
-  y = np.arange(y_length)
-  X, Y = np.meshgrid(x, y)
+
 
   x_dir = np.gradient(x_map, 1, 1)
   y_dir = np.gradient(y_map, 1, 1)
@@ -105,7 +102,7 @@ def jacobian_det(x_map, y_map):
 
 
 
-def create_grid_sample(x_length: int, y_length: int, A_nm: np.array, B_nm: np.array) -> t.Tensor:
+def create_grid_sample(x_length: int, y_length: int, A_list: np.array, B_list: np.array) -> t.Tensor:
   '''
   cos distortion for t.nn.functional.grid_sample, using cos b/c the grid is from -1 to 1
 
@@ -119,90 +116,94 @@ def create_grid_sample(x_length: int, y_length: int, A_nm: np.array, B_nm: np.ar
   - x_map (np.array): Size `x_length` * `y_length`.
   - y_map (np.array): Size `x_length` * `y_length`.
   '''
-  ### Checks, pads to correct size with zero
-  # if A_nm.shape != (x_length, y_length):
-  #   A_nm = np.pad(A_nm, ((0, x_length - A_nm.shape[0]),(0, y_length - A_nm.shape[1])), 'constant')
-  # if B_nm.shape != (x_length, y_length):
-  #   B_nm = np.pad(B_nm, ((0, x_length - B_nm.shape[0]),(0, y_length - B_nm.shape[1])), 'constant')
+  flow_grid = []
+
+  for A_nm, B_nm in zip(A_list,B_list):
+
+    # find non-zero coefficient and convert to frequency
+    non_zero_A_arg = np.nonzero(A_nm)
+    freq_A_arg = (np.array(non_zero_A_arg) + 1) * np.pi / 2  # n pi / L
+    max_A_freq = np.max(np.transpose(freq_A_arg), axis = 1)
+    non_zero_B_arg = np.nonzero(B_nm)
+    freq_B_arg = (np.array(non_zero_B_arg) + 1) * np.pi / 2
+    max_B_freq = np.max(np.transpose(freq_B_arg), axis = 1)
+
+    # save time by only computing non-duplicate index
+    unique_A_x, inv_index_A_x = np.unique(freq_A_arg[1], return_inverse = True)
+    unique_A_y, inv_index_A_y = np.unique(freq_A_arg[0], return_inverse = True)
+    unique_B_x, inv_index_B_x = np.unique(freq_B_arg[1], return_inverse = True)
+    unique_B_y, inv_index_B_y = np.unique(freq_B_arg[0], return_inverse = True)
+
+    # Create Coordinates
+    x = np.linspace(-1, 1, num = x_length, endpoint = True)
+    y = np.linspace(-1, 1, num = y_length, endpoint = True)
+    X, Y = np.meshgrid(x, y, indexing='ij') # first index is x and second y
+
+    # Normalization
+    # max_length = max((x_length - 1), (y_length - 1))
+    normalization_A = 1 / (max_A_freq)
+    normalization_B = 1 / (max_B_freq)
+    #np.nan_to_num(normalization_A, copy = False, nan = 1)
+    #np.nan_to_num(normalization_B, copy = False, nan = 1)
+
+    # basis to do outer product, truncate smaller than 1e-15
+    x_basis_A = np.sin(unique_A_x[None, None, :] * (X[:, :, None] + 1))
+    y_basis_A = np.sin(unique_A_y[None, None, :] * (Y[:, :, None] + 1))
+    x_basis_B = np.sin(unique_B_x[None, None, :] * (X[:, :, None] + 1))
+    y_basis_B = np.sin(unique_B_y[None, None, :] * (Y[:, :, None] + 1))
+
+    eps = np.finfo(np.float64).eps * 10
+    x_basis_A[np.abs(x_basis_A) < eps] = 0
+    y_basis_A[np.abs(y_basis_A) < eps] = 0
+    x_basis_B[np.abs(x_basis_B) < eps] = 0
+    y_basis_B[np.abs(y_basis_B) < eps] = 0
+
+    # fully vectorized fourier sum
+    X_pert = np.einsum('xyi, i, xyi, i -> xy', y_basis_A[:,:,inv_index_A_y], A_nm[non_zero_A_arg], x_basis_A[:,:,inv_index_A_x], normalization_A)
+    Y_pert = np.einsum('xyi, i, xyi, i -> xy', y_basis_B[:,:,inv_index_B_y], B_nm[non_zero_B_arg], x_basis_B[:,:,inv_index_B_x], normalization_B)
 
 
-  # find non-zero coefficient and convert to frequency
-  non_zero_A_arg = np.nonzero(A_nm)
-  freq_A_arg = (np.array(non_zero_A_arg) + 1) * np.pi / 2  # n pi / L
-  max_A_freq = np.max(np.transpose(freq_A_arg), axis = 1)
-  non_zero_B_arg = np.nonzero(B_nm)
-  freq_B_arg = (np.array(non_zero_B_arg) + 1) * np.pi / 2
-  max_B_freq = np.max(np.transpose(freq_B_arg), axis = 1)
+    x_map = (X + X_pert)
+    y_map = (Y + Y_pert)
 
-  # save time by only computing non-duplicate index
-  unique_A_x, inv_index_A_x = np.unique(freq_A_arg[1], return_inverse = True)
-  unique_A_y, inv_index_A_y = np.unique(freq_A_arg[0], return_inverse = True)
-  unique_B_x, inv_index_B_x = np.unique(freq_B_arg[1], return_inverse = True)
-  unique_B_y, inv_index_B_y = np.unique(freq_B_arg[0], return_inverse = True)
+    flow_grid_np = np.stack((np.float32(y_map), np.float32(x_map)), axis = -1)
+    flow_grid.append(t.from_numpy(flow_grid_np).unsqueeze(0))
 
-  # Create Coordinates
-  x = np.linspace(-1, 1, num = x_length, endpoint = True)
-  y = np.linspace(-1, 1, num = y_length, endpoint = True)
-  X, Y = np.meshgrid(x, y, indexing='ij') # first index is x and second y
+  flow_grid = t.cat(flow_grid, dim = 0)
+  
+  return flow_grid
 
-  # Normalization
-  # max_length = max((x_length - 1), (y_length - 1))
-  normalization_A = 1 / (max_A_freq)
-  normalization_B = 1 / (max_B_freq)
-  #np.nan_to_num(normalization_A, copy = False, nan = 1)
-  #np.nan_to_num(normalization_B, copy = False, nan = 1)
+class get_diffeomorphism:
+  def __init__(self, x_res: int, y_res: int, A = None, B = None, rng = None, seed = 37):
+    self.x_res = x_res
+    self.y_res = y_res
+    if rng == None:
+      self.rng = np.random.default_rng(seed = seed)
+    else: 
+      self.rng = rng 
+    self.A = A
+    self.B = B
+    if A == None: self.A = []
+    if B == None: self.B = []
+    self.diffeos = []
+    self.sparse_AB_param = []
 
-  # basis to do outer product, truncate smaller than 1e-15
-  x_basis_A = np.sin(unique_A_x[None, None, :] * (X[:, :, None] + 1))
-  y_basis_A = np.sin(unique_A_y[None, None, :] * (Y[:, :, None] + 1))
-  x_basis_B = np.sin(unique_B_x[None, None, :] * (X[:, :, None] + 1))
-  y_basis_B = np.sin(unique_B_y[None, None, :] * (Y[:, :, None] + 1))
+  def sparse_AB_append(self, x_cutoff, y_cutoff, num_of_terms, diffeo_amp, num_of_diffeo, rng = None, seed = 37, alpha = None):
+    if rng == 'self': rng = self.rng
+    A_nm, B_nm = sparse_transform_amplitude(x_cutoff, y_cutoff, num_of_terms, diffeo_amp, loop = num_of_diffeo, rng = rng, seed = seed, alpha = alpha)
+    
+    self.sparse_AB_param.append({'x_cutoff': x_cutoff, 'y_cutoff': y_cutoff, 'num_of_diffeo':num_of_diffeo, 'diffeo_amp':diffeo_amp, 'sparsity': num_of_terms, 'rng':rng, 'seed':seed, 'alpha':alpha})
+    self.A.append(A_nm)
+    self.B.append(B_nm)
+  
+  def get_all_grid(self):
+    self.diffeos = []
+    for A, B in zip(self.A, self.B):
+      self.diffeos.append(create_grid_sample(self.x_res, self.y_res, A, B))
+  
+  def __getitem__(self, index):
+    return self.diffeos[index[0]][index[1:]]
 
-  eps = np.finfo(np.float64).eps * 10
-  x_basis_A[np.abs(x_basis_A) < eps] = 0
-  y_basis_A[np.abs(y_basis_A) < eps] = 0
-  x_basis_B[np.abs(x_basis_B) < eps] = 0
-  y_basis_B[np.abs(y_basis_B) < eps] = 0
-
-  # fully vectorized fourier sum
-  X_pert = np.einsum('xyi, i, xyi, i -> xy', y_basis_A[:,:,inv_index_A_y], A_nm[non_zero_A_arg], x_basis_A[:,:,inv_index_A_x], normalization_A)
-  Y_pert = np.einsum('xyi, i, xyi, i -> xy', y_basis_B[:,:,inv_index_B_y], B_nm[non_zero_B_arg], x_basis_B[:,:,inv_index_B_x], normalization_B)
-
-
-  x_map = (X + X_pert)
-  y_map = (Y + Y_pert)
-
-  # x_map_invert = (X - X_pert) # creating array with correct shape
-  # y_map_invert = (Y - Y_pert) 
-
-  # x_map_invert[1:-1, 1:-1] = (X[1:-1, 1:-1] + X_pert[1:-1, 1:-1] * (
-  #   (np.diff(x_map, axis = 0) / np.diff(X, axis = 0))[0:-1, 1:-1] * (X_pert[1:-1, 1:-1] > 0) 
-  #   + (np.diff(x_map, axis = 0) / np.diff(X, axis = 0))[1:  , 1:-1] * (X_pert[1:-1, 1:-1] < 0)))
-  # y_map_invert[1:-1, 1:-1] = (Y[1:-1, 1:-1] + Y_pert[1:-1, 1:-1] * (
-  #   (np.diff(y_map, axis = 1) / np.diff(Y, axis = 1))[1:-1, 0:-1] * (Y_pert[1:-1, 1:-1] > 0) 
-  #   + (np.diff(y_map, axis = 1) / np.diff(Y, axis = 1))[1:-1, 1:  ] * (Y_pert[1:-1, 1:-1] < 0)))
-
-  #x_map_invert, y_map_invert = interpolate_perturbations(x_map_invert, y_map_invert, x_map, y_map)
-
-
-
-  # flow_grid = np.stack((x_map, y_map), axis = 0)
-  # flow_grid = t.from_numpy(flow_grid).unsqueeze(0)
-
-  # flow_grid_invert = np.stack((x_map_invert, y_map_invert), axis = -1)
-  # flow_grid_invert = t.from_numpy(flow_grid_invert).unsqueeze(0)
-  # flow_grid_invert = t.nn.functional.grid_sample(flow_grid, flow_grid_invert)
-
-  # flow_grid_invert = t.permute(flow_grid_invert, (0, 2, 3, 1))
-  # flow_grid = t.permute(flow_grid, (0, 2, 3, 1))
-
-  flow_grid_np = np.stack((np.float32(y_map), np.float32(x_map)), axis = -1)
-  flow_grid = t.from_numpy(flow_grid_np).unsqueeze(0)
-
-  # flow_grid_inverse_np = np.stack((np.float32(x_map_invert), np.float32(y_map_invert)), axis = -1)
-  # flow_grid_inverse = t.from_numpy(flow_grid_inverse_np).unsqueeze(0)
-
-  return flow_grid #, flow_grid_inverse
 
 class BiasOnly_gridInv(nn.Module):
   def __init__(self, grid):
